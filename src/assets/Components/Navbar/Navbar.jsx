18bfefb5ead3f494ed347f0713a26e90e773475e
Mishal Fatima <116995325+fmishal099@users.noreply.github.com>
2024-10-07 12:19:27 +0500

    initial commit

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -14,22 +14,25 @@ const Navbar = () => {
       <div className="navbar-logo">
         <h2>Portfolio</h2>
       </div>
+      
       <div className={`navbar-items ${isOpen ? 'navbar-open' : ''}`}>
         <ul>
-        <li><a href="#home">Home</a></li>
-  <li><a href="#about">About Me</a></li>
-  <li><a href="#services">Services</a></li>
-  <li><a href="#projects">Projects</a></li>
-  <li><a href="#contact">Contact</a></li>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
+
       <div>
-      <button className="navbar-btn">
-  <a href="#" className="navbar-link">
-    <FaWhatsapp className="whatsapp-icon" /> Whatsapp
-  </a>
-</button>
+        <button className="navbar-btn">
+          <a href="#" className="navbar-link">
+            <FaWhatsapp className="whatsapp-icon" /> Whatsapp
+          </a>
+        </button>
       </div>
+
       <div className="navbar-hamburger" onClick={toggleMenu}>
         {isOpen ? (
           <span className="navbar-close-icon">&times;</span>
@@ -43,6 +46,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
